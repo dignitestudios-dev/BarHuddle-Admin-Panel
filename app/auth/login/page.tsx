bff8cfa5e2688c1f03b22dbbafd27e645cccb227
zackcole <zeeshan.ahmed@launchbox.pk>
2026-07-13 00:51:13 -0700

    update:admin panel completed

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -73,6 +73,11 @@ const Login = () => {
           />
         </div>
 
+
+
+
+
+
         <div className="space-y-2">
           <Label htmlFor="password">Password</Label>
           <div className="relative">
